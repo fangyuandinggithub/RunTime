30d11b0553110cbe377eaf03dad70d7aa677729e
fangyuandinggithub <3026354242@qq.com>
2019-12-17 17:25:19 +0800

    Initial Commit

---
+++ RunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTime
+//
+//  Created by fangyuan ding on 2019/12/17.
+//  Copyright © 2019年 fangyuan ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
